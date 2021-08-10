adc5350a44d3efe2b30b84fe4816f67d96ee75c4
development6 <40081976+development6@users.noreply.github.com>
2021-08-10 03:42:25 -0400

    vyy

--- Hola/src/cl/nikza/hola/Hola.java
+++ Hola/src/cl/nikza/hola/Hola.java
@@ -3,7 +3,7 @@ package cl.nikza.hola;
 public class Hola {
 
 	public static void main(String[] args) {
-		System.out.println("Hola a todos coo estan");
+		System.out.println("Hola a todos coo es  tan");
 
 	}
 
